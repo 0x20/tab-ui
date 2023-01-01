bbdfe2ad285aecc9cd6a7ac5c1119107d659726e
Yvan Janssens <yvanj@cyber.gent>
2023-01-01 14:29:37 +0100

    Change UI to allow it to work on larger displays.
    This commit removes the debug elements that are off-screen and centers the panel so it works on screens larger than 1024x768.

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     qputenv("QT_IM_MODULE", QByteArray("qtvirtualkeyboard"));
-    
+
     QGuiApplication app(argc, argv);
 
     qmlRegisterType<TqMemberModel>("com.thequux.tab", 1, 0, "MemberModel");
